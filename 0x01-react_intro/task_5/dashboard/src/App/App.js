8965e1b78b536a54ce34acdc3ef966320eadbb70
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-09-21 21:03:25 +0100

    Reorganize the files

---
+++ 0x01-react_intro/task_5/dashboard/src/App/App.js
@@ -0,0 +1,29 @@
+import logo from './holberton-logo.jpg'
+import './App.css';
+import { getFullYear, getFooterCopy } from '../utils/utils';
+
+function App() {
+  return (
+    <>
+      <div className="App-header">
+        <img src={logo} alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label>Email
+          <input type="email" />
+        </label>
+        <label >Password
+          <input type="password" />
+        </label>
+        <button type="button">OK</button>
+      </div>
+      <div className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
