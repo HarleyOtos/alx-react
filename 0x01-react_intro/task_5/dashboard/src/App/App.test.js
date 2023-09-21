da648292d0ab249a859e407c817f94667cad433c
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-09-21 21:22:57 +0100

    Testing

--- 0x01-react_intro/task_5/dashboard/src/App/App.test.js
+++ 0x01-react_intro/task_5/dashboard/src/App/App.test.js
@@ -1,5 +1,6 @@
 import { shallow } from "enzyme";
-import App from "./App/App";
+import { React } from "react";
+import App from "./App";
 
 describe('<App />', () => {
   it('App renders without crashing', () => {
