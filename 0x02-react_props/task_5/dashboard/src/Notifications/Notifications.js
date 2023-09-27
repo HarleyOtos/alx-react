f6c32c1e87804142ca51503a5de1f3b856712e4a
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-09-27 14:28:29 +0100

    notification array updated

--- 0x02-react_props/task_5/dashboard/src/Notifications/Notifications.js
+++ 0x02-react_props/task_5/dashboard/src/Notifications/Notifications.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import PropTypes from "prop-types";
+import { getLatestNotification } from '../utils/utils';
+import NotificationItemShape from './NotificationItemShape';
 import './Notifications.css';
 import closeIcon from '../assets/close-icon.png';
-import { getLatestNotification } from '../utils/utils';
 import NotificationItem from './NotificationItem';
 
-const Notifications = ({ displayDrawer }) => {
-  
+const Notifications = ({ displayDrawer, listNotifications }) => {
+
   Notifications.defaultProps = {
     displayDrawer: false,
+    listNotifications: [],
   };
 
   Notifications.propTypes = {
     displayDrawer: PropTypes.bool,
+    listNotifications: PropTypes.arrayOf(NotificationItemShape),
   };
 
   return (
@@ -21,28 +24,40 @@ const Notifications = ({ displayDrawer }) => {
         <p>Your Notifications</p>
       </div>
       {displayDrawer && (
-      <div className='Notifications'>
-        <button
-          style={{
-            background: "transparent",
-            border: 'none',
-            position: 'absolute',
-            right: 20,
-          }}
-          aria-label='close'
-        >
-          <img src={closeIcon} alt='close-icon' />
-        </button>
-        <p>Here is the list of notifications</p>
-        <ul>
-          <NotificationItem type="default" value="New course available" />
-          <NotificationItem type="urgent" value="New resume available" />
-          <NotificationItem
-            type="urgent"
-            html={{ __html: getLatestNotification() }}
-          />
-        </ul>
-      </div>
+        <div className='Notifications'>
+          <button
+            style={{
+              background: "transparent",
+              border: 'none',
+              position: 'absolute',
+              right: 20,
+            }}
+            aria-label='close'
+          >
+            <img src={closeIcon} alt='close-icon' />
+          </button>
+          <p>Here is the list of notifications</p>
+          <ul>
+            {listNotifications.length === 0 && (
+              <NotificationItem value="No new notification for now" />
+            )}
+
+            {listNotifications.map((notification) => (
+              <NotificationItem
+                key={notification.id}
+                type={notification.type}
+                value={notification.value}
+                html={notification.html}
+              />
+
+            ))}
+            <NotificationItem type="urgent" value="New resume available" />
+            <NotificationItem
+              type="urgent"
+              html={{ __html: getLatestNotification() }}
+            />
+          </ul>
+        </div>
       )}
     </>
   );
