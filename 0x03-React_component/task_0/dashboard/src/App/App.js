9ff0239eab1f48be3ffd5c243ad3ecf805da51cd
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-10-29 23:39:44 +0100

    requirement debug & fix

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Notifications from '../Notifications/Notifications';
 import Header from '../Header/Header';
@@ -8,8 +8,19 @@ import CourseList from '../CourseList/CourseList';
 import { getLatestNotification } from "../utils/utils";
 import "./App.css";
 
+const listCourses = [
+  { id: 1, name: "ES6", credit: 60 },
+  { id: 2, name: "Webpack", credit: 20 },
+  { id: 3, name: "React", credit: 40 },
+];
 
-class App extends React.component {
+const listNotifications = [
+  { id: 1, type: "default", value: "New course available" },
+  { id: 2, type: "urgent", value: "New resume available" },
+  { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
+];
+
+class App extends Component {
   render() {
     const { isLoggedIn } = this.props;
     return (
@@ -34,18 +45,6 @@ class App extends React.component {
   }
 }
 
-const listCourses = [
-  { id: 1, name: "ES6", credit: 60 },
-  { id: 2, name: "Webpack", credit: 20 },
-  { id: 3, name: "React", credit: 40 },
-];
-
-const listNotifications = [
-  { id: 1, type: "default", value: "New course available" },
-  { id: 2, type: "urgent", value: "New resume available" },
-  { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
-];
-
 App.defaultProps = {
   isLoggedIn: false,
 };
@@ -53,6 +52,5 @@ App.defaultProps = {
 App.propTypes = {
   isLoggedIn: PropTypes.bool,
 };
-
 
 export default App;
