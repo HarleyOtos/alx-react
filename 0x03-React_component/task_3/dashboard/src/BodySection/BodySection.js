5d5438719347700d504bb70b0065fb75d07e1dd4
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-10-30 14:51:58 +0100

    bodySection css imported

--- 0x03-React_component/task_3/dashboard/src/BodySection/BodySection.js
+++ 0x03-React_component/task_3/dashboard/src/BodySection/BodySection.js
@@ -1,5 +1,6 @@
-import React, {Component} from 'react'
-import PropTypes from 'prop-types'
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import './BodySection.css';
 
 class BodySection extends Component {
   constructor(props) {
