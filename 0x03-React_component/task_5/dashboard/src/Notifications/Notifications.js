52fedd149696b64316a438e2ba8b1b0f6a2546e0
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-11-05 22:52:20 +0100

    Declare a pure component

--- 0x03-React_component/task_5/dashboard/src/Notifications/Notifications.js
+++ 0x03-React_component/task_5/dashboard/src/Notifications/Notifications.js
@@ -17,6 +17,12 @@ class Notifications extends Component {
     console.log(`Notification ${id} has been marked as read`);
   }
 
+  shouldComponentUpdate(nextProps) {
+    return (
+      nextProps.listNotifications.length > this.props.listNotifications.length
+    );
+  }
+
   render() {
     const { displayDrawer, listNotifications } = this.props;
 
