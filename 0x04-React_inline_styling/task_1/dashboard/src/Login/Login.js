8b156c9fe9f23a16c043387bf186d503042a5e92
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-11-06 00:17:38 +0100

    login modified

--- 0x04-React_inline_styling/task_1/dashboard/src/Login/Login.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Login/Login.js
@@ -1,21 +1,33 @@
 import React from 'react'
-import './Login.css'
+import { StyleSheet, css } from "aphrodite";
 
 function Login() {
   return (
     <>
-      <div className="login">
+      <div className={css(styles.login)}>
         <p>Login to access the full dashboard</p>
         <label>Email
-          <input type="email" id='email' />
+          <input type="email" id='email' className={css(styles.loginInput)} />
         </label>
         <label >Password
-          <input type="password" id='password' />
+          <input type="password" id='password' className={css(styles.loginInput)}/>
         </label>
         <button type="button">OK</button>
       </div>
     </>
-  )
+  );
 }
 
-export default Login
+const styles = StyleSheet.create({
+  login: {
+    margin: "50px",
+    flexGrow: 1,
+  },
+
+  loginInput: {
+    marginLeft: "10px",
+    marginRight: "20px",
+  },
+});
+
+export default Login;
