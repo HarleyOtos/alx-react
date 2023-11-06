e02cd05323c720a968ecc87ae27c2bc5541ecab6
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-11-06 10:04:01 +0100

    task 3 Responsive design

---
+++ 0x04-React_inline_styling/task_3/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,152 @@
+import React, { Component } from 'react';
+import PropTypes from "prop-types";
+import { getLatestNotification } from '../utils/utils';
+import NotificationItemShape from './NotificationItemShape';
+import closeIcon from '../assets/close-icon.png';
+import NotificationItem from './NotificationItem';
+import { StyleSheet, css } from "aphrodite";
+
+
+class Notifications extends Component {
+  constructor(props) {
+    super(props)
+    this.markAsRead = this.markAsRead.bind(this);
+  }
+
+  markAsRead(id) {
+    console.log(`Notification ${id} has been marked as read`);
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return (
+      nextProps.listNotifications.length > this.props.listNotifications.length
+    );
+  }
+
+  render() {
+    const { displayDrawer, listNotifications } = this.props;
+
+    const menuPStyle = css(
+      displayDrawer ? styles.menuItemPNoShow : styles.menuItemPShow
+    );
+
+    return (
+      <>
+        <div id='menuItem' className={css(styles.menuItem)}>
+          <p>Your Notifications</p>
+        </div>
+        {displayDrawer && (
+          <div id='Notifications' className={css(styles.notifications)}>
+            <button
+              style={{
+                background: "transparent",
+                border: 'none',
+                position: 'absolute',
+                right: 20,
+              }}
+              aria-label='close'
+            >
+              <img src={closeIcon} alt='close-icon'
+                className={css(styles.notificationsButtonImage)}
+              />
+            </button>
+            <p className={css(styles.notificationsP)}>
+              Here is the list of notifications
+            </p>
+            <ul className={css(styles.notificationsUL)}>
+              {listNotifications.length === 0 && (
+                <NotificationItem value="No new notification for now" />
+              )}
+
+              {listNotifications.map((notification) => (
+                <NotificationItem
+                  key={notification.id}
+                  id={notification.id}
+                  type={notification.type}
+                  value={notification.value}
+                  html={notification.html}
+                  markAsRead={this.markAsRead}
+                />
+              ))}
+              <NotificationItem type="urgent" value="New resume available" />
+              <NotificationItem
+                type="urgent"
+                html={{ __html: getLatestNotification() }}
+              />
+            </ul>
+          </div>
+        )}
+      </>
+    );
+  };
+}
+
+Notifications.defaultProps = {
+  displayDrawer: false,
+  listNotifications: [],
+};
+
+Notifications.propTypes = {
+  displayDrawer: PropTypes.bool,
+  listNotifications: PropTypes.arrayOf(NotificationItemShape),
+};
+
+const cssVars = {
+  mainColor: "#e01d3f",
+};
+
+const screenSize = {
+  small: "@media screen and (max-width: 900px)",
+};
+
+const styles = StyleSheet.create({
+  menuItem: {
+    textAlign: "right",
+  },
+
+  menuItemPNoShow: {
+    marginRight: "8px",
+    [screenSize.small]: {
+      display: "none",
+    },
+  },
+
+  menuItemPShow: {
+    marginRight: "8px",
+  },
+
+  notifications: {
+    float: "right",
+    border: `3px dashed ${cssVars.mainColor}`,
+    padding: "10px",
+    marginBottom: "20px",
+    [screenSize.small]: {
+      float: "none",
+      border: "none",
+      listStyle: "none",
+      padding: 0,
+      fontSize: "20px",
+      position: "absolute",
+      background: "white",
+      height: "110vh",
+      width: "100vw",
+    },
+  },
+
+  notificationsButtonImage: {
+    width: "10px",
+  },
+
+  notificationsUL: {
+    [screenSize.small]: {
+      padding: 0,
+    },
+  },
+
+  notificationsP: {
+    margin: 0,
+    marginTop: "15px",
+  },
+});
+
+export default Notifications;
